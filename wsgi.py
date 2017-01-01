63e722c234036248b3e3bf7eb4639e7405610f55
jaakko <jaakko.jokinen@outlook.com>
2017-01-01 20:46:02 +0200

    Remove Django files

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,31 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
+#!/usr/bin/python
 import os
 
-from django.core.wsgi import get_wsgi_application
+virtenv = os.environ.get('OPENSHIFT_PYTHON_DIR', '') + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+from rest.app import app as application
 
-application = get_wsgi_application()
+# OpenShift support: bind to OpenShift environment variables if available
+host = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', '5000'))
+
+if __name__ == '__main__':
+    application.run(host=host, port=port)
+
+#
+# Below for testing only
+#
+# if __name__ == '__main__':
+#     from wsgiref.simple_server import make_server
+#     httpd = make_server('localhost', 8051, application)
+#     # Wait for a single request, serve it and quit.
+#     httpd.serve_forever()
